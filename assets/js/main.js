5ece4908ef893b6753de0132d720a7dbd0d2b53b
Matt Coy <fluffytoycoy@gmail.com>
2019-06-04 22:11:15 -0500

    added scroll animation to titles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,20 @@
 import { Logo } from './animations/logo';
+const ScrollTrigger = require('scrolltrigger-classes');
+
+document.addEventListener('DOMContentLoaded', function() {
+	var trigger = new ScrollTrigger();
+});
 
 $(document).ready(function() {
 	Logo.initLogoSpin();
+
 	$('.project').on('click', function() {
 		$('#pop-up').addClass('visable');
+		$('body').addClass('pop-up');
 	});
 
 	$('.close-btn').on('click', function() {
-		alert('test');
 		$('#pop-up').removeClass('visable');
+		$('body').removeClass('pop-up');
 	});
 });
