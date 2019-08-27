ee0885e51fd98f99fc1e1f7636be8ec8ad031983
Matt Coy <fluffytoycoy@gmail.com>
2019-08-27 03:13:06 -0700

    fixing css

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,16 +8,6 @@ document.addEventListener('DOMContentLoaded', function() {
 $(document).ready(function() {
 	Logo.initLogoSpin();
 
-	$('.project').on('click', function() {
-		$('#pop-up').addClass('visable');
-		$('body').addClass('pop-up');
-	});
-
-	$('.close-btn').on('click', function() {
-		$('#pop-up').removeClass('visable');
-		$('body').removeClass('pop-up');
-	});
-
 	$('.slide-btn').on('click', function() {
 		$('.skill-slide-1').toggleClass('slide-closed');
 		$('.skill-slide-2').toggleClass('slide-open');
